8f89e02a0080ba6b09d67797acbb12f5752e9d2d
Emmanuel Godwin <godwinemmanuel302@gmail.com>
2021-09-08 15:29:03 +0100

    Always use HTTPS

--- resources/views/pages/registered-members.blade.php
+++ resources/views/pages/registered-members.blade.php
@@ -24,42 +24,18 @@
             </thead>
 
             <tbody>
-                @for ($i = 0; $i < 10; $i++)
+                @for ($i = 0; $i < 5; $i++)
                 <tr>
                     <td class="member-name">JOHN</td>
                     <td class="member-lastnames">Jonas Marc</td>
-                    <td>+22997979797</td>
-                    <td>Abomey-Calavi</td>
-                    <td member-email>100.000</td>
-                    <td member-contact>Offrande</td>
+                    <td class="member-tel">+22997979797</td>
+                    <td class="member-email">Abomey-Calavi</td>
+                    <td class="member-email">100.000</td>
+                    <td class="member-contact">Offrande</td>
                 </tr> 
                 @endfor
             </tbody>
         </table>
-
-        <div class="members-container">
-
-            @for ($i = 0; $i < 5; $i++)
-                <div class="member my-3">
-                    <div class="member-profile-picture">
-                        <img src="{{ asset("images/MaleAvatar.jpg") }}" alt="PICTURE" >
-                    </div>
-                    <div class="member-infos">
-                        <div class="member-fullname fw-bold">Jonas JOHN</div>
-                        <a class="member-email text-primary" href="mailto:adresse@gmail.com">adresse@gmail.com</a>
-                        <span class="d-flex justify-content-between">
-                            <a href="tel:+229 97979797" class="member-contact">+22997979797</a>
-                            <small class="fst-italic fw-bold"> Inscrit le 07/09/2021</small>
-                        </span>
-                    </div>
-                    <div class="config-member-button">
-                        <h5 class="fas fa-user-lock text-warning"></h5>
-                        <h5 class="fas fa-user-slash text-danger"></h5>
-                    </div>
-                </div>
-            @endfor
-
-        </div>
     </div>
 
 @endsection
